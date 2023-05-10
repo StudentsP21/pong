68e2bc5b2c7f43233143aca3eec39a24e57b14fa
deustenebrarum <gamesevolutionteam@gmail.com>
2023-05-10 17:29:06 +0300

    Game Loop precomplete

--- check_ball.hpp
+++ check_ball.hpp
@@ -1,20 +1,16 @@
 #pragma once
 
-enum field_side {
-    RIGHT,
-    LEFT
-};
+#include "FieldSide.h"
 
 bool check_hit(int field_width, int ball_x) {
-
     if (ball_x == 1 || ball_x == field_width)
         return true;
     return false;
 }
 
-field_side ball_side(int field_width, int ball_x) {
+FieldSide ball_side(int field_width, int ball_x) {
     if (ball_x > field_width / 2) {
-        return RIGHT;
+        return FieldSide::RIGHT;
     }
-    return LEFT;
+    return FieldSide::LEFT;
 }
--- game_loop.h
+++ game_loop.h
@@ -1,7 +1,31 @@
 #pragma once
 
+//счЄтчик повторов цикла игры
+
+//сделать счЄтчик времени проведЄнный в игре (без нажатой паузы)
+
+//сделать паузу игры и счЄтчик времени 
+
+//при попадании м€ча 1 игроку добавл€ть 1 балл 2 игроку и наоборот
+
+//при попадании возвращ€ть м€чь на центр пол€ 
+
+//при набирании 3 очков у 1 или 2 игрока останавливать игру
+
 #include <cstdint>
+#include <chrono>
+#include <thread>
+
+using namespace std::chrono_literals;
+
+#include "check_ball.hpp"
 #include "FieldSide.h"
+#include "Direction.h"
+
+constexpr uint64_t FPS = 5;
+
+constexpr int field_height = 30;
+constexpr int field_width = 61;
 
 bool is_time_for_turn(
 	uint64_t frames_count,
@@ -14,31 +38,53 @@ bool is_time_for_turn(
 	return false;
 }
 
+void reset_all(int& player_y, int& enemy_y, 
+	int& ball_x, int& ball_y, Direction& ball_direction) {
+	player_y = field_height / 2;
+	enemy_y = field_height / 2;
+
+	ball_x = field_width / 2 + 1;
+	ball_y = field_height / 2;
+	ball_direction = rand() % 2 == 0 ?
+		Direction::LEFT : Direction::RIGHT;
+}
+
 void game_loop() {
+	int player_y, enemy_y;
+
+	int ball_x, ball_y;
+	Direction ball_direction;
+
+	reset_all(player_y, enemy_y, ball_x, ball_y, ball_direction);
+
 	uint64_t frames_count = 0;
-	const uint64_t FPS = 5;
 
-	const int platform_delay_frames_count = 2;
+	constexpr int win_score = 3;
 
-	while (true) {
+	constexpr int platform_turns_per_second = 2;
+	constexpr int ball_turns_per_second = 5;
+
+	for (;;) {
 		if (is_time_for_turn(
 			frames_count, 
 			FPS,
-			platform_delay_frames_count
+			platform_turns_per_second
 		)) {
-			if (check_player_moving())
-				move(player_y, handle_moving_direction());
-			bot_move();
+			handle_player_control(player_y);
+			bot_move(enemy_y, ball_y);
 		}
 
 		if (is_time_for_turn(
 			frames_count,
 			FPS,
-			ball_delay_frames_count
+			ball_turns_per_second
 		)) {
-			ball_move();
+			ball_move(ball_y, ball_x, ball_direction);
 			if (check_hit(field_width, ball_x)) {
 				FieldSide side = ball_side(field_width, ball_x);
+
+				reset_all(player_y, enemy_y, ball_x, ball_y, ball_direction);
+
 				switch (side) {
 				case LEFT:
 					++right_score;
@@ -53,7 +99,15 @@ void game_loop() {
 				break;
 		}
 
+		print_field(
+			player_y, enemy_y, 
+			ball_x, ball_y, ball_direction,
+			field_height, field_width
+		);
+
 		++frames_count;
+
+		std::this_thread::sleep_for(1000/FPS*1ms);
 	}
 
 	if (left_score == win_score) {
