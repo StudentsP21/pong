68eb83936d5afbf4eb5723101280169322c64053
Ziegler 7 <fetkorro@gmail.com>
2023-05-10 18:18:58 +0300

    Add Player Control

---
+++ handle_player_control.hpp
@@ -0,0 +1,20 @@
+﻿
+#include <Windows.h>
+
+bool isKeyDown(char key) {
+	return GetKeyState(key) & 0x8000;
+}
+
+void handler_player_control(int& player_y) {
+	if (isKeyDown(VK_UP))
+	{
+		player_y--;
+	}
+	if (isKeyDown(VK_DOWN))
+	{
+		player_y++;
+	}
+}
+
+
+
